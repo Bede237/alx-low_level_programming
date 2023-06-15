b336a860743dddb124d9a2977ac430c9ce7cd2ed
Bede237 <younkweb@gmail.com>
2023-06-15 01:30:33 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * the main - Entry point
+ *
+ * Return - Always 0 (success)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"); /* out puts a string */
+	return (0);
+}
