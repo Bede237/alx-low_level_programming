933aabef4be9a647b2bb4e5244f4ff2f36e57f93
Bede237 <younkweb@gmail.com>
2023-06-15 01:42:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); /* out puts a string */
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
