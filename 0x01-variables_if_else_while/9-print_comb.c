7787cd0ede3ea724ac711bc688e1949821e6c9eb
Bede237 <younkweb@gmail.com>
2023-06-16 19:11:06 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,16 @@ int main(void)
 
 	for (a = 0; a <= 9; a++)
 	{
+		putchar(a + '0');
 		if (a == 9)
 		{
-			putchar(a + '0');
-			 putchar('\n');
+			break;
 		} else
 		{
-			putchar(a + '0');
-			putchar(", ");
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
