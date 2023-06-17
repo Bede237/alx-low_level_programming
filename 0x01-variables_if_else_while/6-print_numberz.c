658c44006b68266317765738828edd2c76c381d5
Bede237 <younkweb@gmail.com>
2023-06-16 17:02:56 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(int a);
+	}
+	putchar('\n');
+	return (0);
+}
