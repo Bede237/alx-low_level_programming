9f65cb5758f13a0aa9220eb394fe3e87cc6df9c0
Bede237 <younkweb@gmail.com>
2023-06-23 18:01:00 -0700

    continuation of nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit: Test if input is a digit or not
+ * @c: input to be checked
+ * Return: 1 is true, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul: multiplies two numbers
+ * @a: input 1 to mul
+ * @b: input 2 to mul
+ * Return: returs (a*b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
