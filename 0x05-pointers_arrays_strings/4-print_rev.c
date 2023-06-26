fdf2edee51fc3a95bf73fe95aee5714aac4d1a68
Bede237 <younkweb@gmail.com>
2023-06-26 15:35:30 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - ouputs a string in reverse
+ * @s: input string
+ */
+void print_rev(char *s)
+{
+        int len = strlen(s);
+        int i;
+
+        for (i = (len - 1); i >= 0; i--)
+        {
+                _putchar(s[i]);
+        }
+        _putchar('\n');
+}
