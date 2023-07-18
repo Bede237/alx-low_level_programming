2791f7e756ab2774f5f9bb7182142675d61bbe74
Bede237 <younkweb@gmail.com>
2023-07-17 18:34:05 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - holds the info of a dog
+ * @name: name of the dog
+ * @age: dogs age
+ * @owner: name of owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog;
