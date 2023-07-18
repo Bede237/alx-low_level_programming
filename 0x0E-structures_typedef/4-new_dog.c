a3df600efad26d17460fd1c398f92da71cc9d534
Bede237 <younkweb@gmail.com>
2023-07-18 01:57:58 -0700

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - creates new struct
+ * @name: member type name
+ * @age: member type age
+ * @owner: member type owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *d;
+
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+}
