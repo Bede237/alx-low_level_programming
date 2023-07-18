b7eaf3463e940830ff0e891cb39b136705897cf5
Bede237 <younkweb@gmail.com>
2023-07-18 02:04:59 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,11 +10,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
-	d = malloc(sizeof(struct dog));
+	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+
 	return (d);
 }
