006331e2ddeafc1e9b0a9f1f12d79f43f43d8d4a
Bede237 <younkweb@gmail.com>
2023-07-17 18:38:32 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - holds the info of a dog
  * @name: name of the dog
@@ -10,3 +13,5 @@ typedef struct dog
 	float age;
 	char *owner;
 }dog;
+
+#endif
