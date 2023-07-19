03d542733f9fbcb3d9450bd7003ef5252985c850
Bede237 <younkweb@gmail.com>
2023-07-19 03:36:26 -0700

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,12 +21,12 @@ int (*get_op_func(char *s))(int, int)
 
 	while(i < 5)
 	{
-		if(ops[i]->op == s[0])
+		if(((ops + i)->op)[0] == s[0])
 		{
 			return (ops[i]->f);
 		}
 
 		i++;
 	}
-	return (ops[6]->f);
+	return (NULL);
 }
