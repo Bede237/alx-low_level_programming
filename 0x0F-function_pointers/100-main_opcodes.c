8853f95515c7ee2b99335947e2253f4fc6c4b85c
Bede237 <younkweb@gmail.com>
2023-07-20 16:26:21 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints opcodes
+ * @argc: number of cmd line args
+ * @argv: list of cmd args
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if(argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	i = atoi(argv[1]);
+
+	if (i < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	return (0);
+}
