f9f3cde1377b4dde90a4d1b6f99771ce4aaa4875
Bede237 <younkweb@gmail.com>
2023-08-07 15:06:07 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,11 +19,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-	{
-		creat(filename, S_IRUSR|S_IWUSR);
-	}
+		text_content = "";
 
-	n = open(filename, O_CREAT|O_WRONLY|O_TRUNC, S_IRUSR|S_IWUSR);
+	n = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (n == -1)
 		return (-1);
